2710e075ccc04af50df074ed8623169c1fa72979
Obar <ducwebdev@gmail.com>
2023-11-08 09:41:02 +0700

    save frame

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,19 @@
+// import { Col, Container, Row } from 'react-bootstrap'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+
+const Login = () => {
+  return (
+    <Container fluid>
+      <Row>
+        <Col>
+          <svg viewBox="0 0 24 24" aria-hidden="true"><g><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path></g></svg>
+        </Col>
+        <Col>2 of 2</Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Login
